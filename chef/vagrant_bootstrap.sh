8b59dc9081e0d88c967b792e2ba5f3b6d5db8fc8
Kostyantyn <kostya.stepanyuk@gmail.com>
2013-12-29 22:33:59 +0100

    Set 0600 for .pem file

--- chef/vagrant_bootstrap.sh
+++ chef/vagrant_bootstrap.sh
@@ -3,5 +3,5 @@
 cd `dirname $0`
 bundle install
 vagrant up
-chmod 0644 .chef/insecure_private_key
+chmod 0600 .chef/insecure_private_key
 bundle exec knife solo bootstrap vagrant@localhost -i .chef/insecure_private_key -p 2222
