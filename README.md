cff9e43ef684fac1cf8db2095d70a439d0aabe5d
Kostyantyn <kostya.stepanyuk@gmail.com>
2013-12-30 13:57:19 +0100

    Create tmp folder during bootstrapping app

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 1. Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
 2. Install [Vagrant](http://www.vagrantup.com/downloads.html)
 3. Clone project ```git clone git@github.com:kostyantyn/hydra_attribute-demo.git```
-4. Run bash script ```cd hydra_attribute-demo && ./chef/vagrant_bootstrap.sh```.
+4. Run bash script ```cd hydra_attribute-demo && ./chef/bootstrap_vagrant.sh```.
    Notice, this script might ask you to enter root password.
    This is a **Vagrant** requirement to export NFS share folder.
    This command can take a while so be patient.
--- chef/site-cookbooks/app/recipes/unicorn.rb
+++ chef/site-cookbooks/app/recipes/unicorn.rb
@@ -5,9 +5,15 @@ rvm_shell 'kill -QUIT `cat tmp/pids/unicorn.pid`' do
   code 'kill -QUIT `cat tmp/pids/unicorn.pid`'
 end
 
-rvm_shell 'unicorn -c config/unicorn.rb -D' do
-  cwd         '/var/www/demo'
-  user        node['rvm']['user_installs'][0]['user']
-  ruby_string node['rvm']['user_installs'][0]['default_ruby']
-  code        'unicorn -c config/unicorn.rb -D'
+[
+  'rake tmp:clear',
+  'rake tmp:create',
+  'unicorn -c config/unicorn.rb -D'
+].each do |command|
+  rvm_shell command do
+    cwd         '/var/www/demo'
+    user        node['rvm']['user_installs'][0]['user']
+    ruby_string node['rvm']['user_installs'][0]['default_ruby']
+    code        command
+  end
 end
